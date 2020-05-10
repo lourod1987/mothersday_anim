b675d7898204ecd57eab98d74e0b98976c67b2ed
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-05-10 02:23:16 -0400

    Feat - add in animation for the heart shape

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.scss';
 
 function App() {
   const opacity = useSpring({ config: { duration: 2500 }, opacity: 1, from: { opacity: 0 }, delay: 3200});
-  const animate1 = useSpring({ config: { duration: 2500 }, from: {marginTop: -2000}, to: { marginTop: 0 }, delay: 200 });
+  const animate1 = useSpring({ config: { duration: 2500 }, from: {marginTop: -2000}, to: { marginTop: 0 }, delay: 500 });
+  const animate2 = useSpring({ config: { duration: 1500 }, from: {marginLeft: -2000}, to: { marginLeft: 0 }, delay: 200 });
 
   return (
     <div className="App">
@@ -12,7 +13,7 @@ function App() {
           <animated.div className="happyday">Happy</animated.div>
           <animated.div className="mother" style={animate1}>Mother's</animated.div>
           <animated.div className="happyday" style={opacity}>Day!</animated.div>
-        <div className="heart-shape"></div>
+        <animated.div className="heart-shape" style={animate2}></animated.div>
       </header>
     </div>
   );
