0793d40cc42384835f7d3cb6e67846744cb0284e
lourod1987 <luisrodrigueziii.dev@gmail.com>
2020-05-10 02:25:36 -0400

    Feat - adjusting heart starting point to ensure it animates off screen on most device screen sizes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.scss';
 function App() {
   const opacity = useSpring({ config: { duration: 2500 }, opacity: 1, from: { opacity: 0 }, delay: 3200});
   const animate1 = useSpring({ config: { duration: 2500 }, from: {marginTop: -2000}, to: { marginTop: 0 }, delay: 500 });
-  const animate2 = useSpring({ config: { duration: 1500 }, from: {marginLeft: -2000}, to: { marginLeft: 0 }, delay: 200 });
+  const animate2 = useSpring({ config: { duration: 1500 }, from: {marginLeft: -3500}, to: { marginLeft: 0 }, delay: 200 });
 
   return (
     <div className="App">
